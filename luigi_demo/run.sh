7e585609793efe490e434ada0cb08da4b98db9d8
Jarlonyan <286568450@qq.com>
2020-03-02 19:57:20 +0800

    update

--- luigi_demo/run.sh
+++ luigi_demo/run.sh
@@ -1,3 +1,9 @@
+export PYTHONPATH=$PATHONPATH:.
 
-
+# 两种调用方式
+# 1.
 luigi --module=hello_luigi SquaredNumbers --local-scheduler
+
+# 2.
+#python hello_luigi.py SquaredNumbers --local-scheduler --n 20
+
